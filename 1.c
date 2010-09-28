4df2f388e942f708a4def4ddc5c27eed0faae329
AnatoliyMashir <ch_584@e1.ru>
2010-09-28 22:45:16 +0600

    with exception

--- 1.c
+++ 1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <errno.h>
 
 int main(){
-    printf("Hello, world!");
+    const char *mess = "my first exeption in c";
+    errno = 0;
+    printf("Hello, world!\r\n");
+    if(errno !=0)
+    {
+	perror(mess);
+    }
     return 1;
 }
