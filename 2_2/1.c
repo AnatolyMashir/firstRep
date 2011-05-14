a76991a304d789bc1d66bad422ce25f3262fc3c4
AnatoliyMashir <ch_584@e1.ru>
2011-05-14 21:12:37 +0600

    ver 2

--- 2_2/1.c
+++ 2_2/1.c
@@ -2,26 +2,44 @@
 #include <stdlib.h>
 #include <signal.h>
 
+const char *printfErrorMessage = "some trouble with printf";
+
 volatile int flag = 1;
 
-void handler()
+void sigHandler(int number, siginfo_t *info, void *something)
 {
-	printf("I catch ctrl+c\n");
-	signal (SIGINT, &handler);
+	printf("Hello, signal %d!\n",info->si_signo);
+	if(number==20)
+		flag=0;
 }
 
-void handler2()
+int main()
 {
-	signal (SIGINT, SIG_DFL);
-	flag = 0;
-}
+	struct sigaction S;
+	int i;
+	
+	S.sa_sigaction = sigHandler;
+	S.sa_flags = SA_SIGINFO;
+	if(sigfillset(&S.sa_mask))
+	{
+		if(fprintf(stderr, "%s\n","some trouble with mask")==-1)
+		{
+			perror(printfErrorMessage);
+			return EXIT_FAILURE;
+		}
+	}
 
-int main(argc, argv)
-int argc;
-char *argv[];
-{
-	signal (SIGINT, &handler);
-	signal (20, &handler2);
+	for(i=1; i<32; ++i)
+		if(i!=9 && i!=19 && sigaction(i, &S, NULL))
+		{
+			if(fprintf(stderr, "%s\n","can't set signal")==-1)
+			{
+				perror(printfErrorMessage);
+				return EXIT_FAILURE;
+			}
+		}
+	
 	while(flag);
-	return 0;
+	return EXIT_SUCCESS;
 }
+
