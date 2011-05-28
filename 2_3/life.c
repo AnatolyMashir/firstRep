2789a02961f76d547f17b8938d248552b9efbf87
AnatoliyMashir <ch_584@e1.ru>
2011-05-28 21:12:53 +0600

    ver 2

--- 2_3/life.c
+++ 2_3/life.c
@@ -14,6 +14,7 @@
 #define error 11
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
+pthread_mutex_t mutex_lock = PTHREAD_MUTEX_INITIALIZER; 
 const char *printfErrorMessage = "some trouble with printf";
 const char *getAffinityMessage = "some trouble with get affinity";
 int ***matrs;
@@ -36,58 +37,61 @@ int mygetch( )
 	tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
 	return ch;
 }
+
+void errorMain(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void errorUser(const char *msg)
+{
+    perror(msg);
+    lock=quit;
+	pthread_exit(&bad);
+}
+
+void errorServer(const char *msg)
+{
+    perror(msg);
+	lock=error;	
+	signal (SIGALRM, SIG_DFL); // чтобы ваша душа была спокойна
+	pthread_exit(&bad);
+}
+
 static void *userInt(void *args)
 {
-	//initscr();
 	int i, j;
 	key = mygetch();
-	//fprintf(stdout, "%s %c\n","user read ", key);
 	while(key!='q')
 	{
 		if(pthread_mutex_lock(&mutex)) // почему лок тут и вообще зачем см. ниже
-		{
-			if(fprintf (stderr, "%s\n","i can't lock mutex")==-1)
-			{
-				perror(printfErrorMessage);
-			}
-			lock=quit;
-			pthread_exit(&bad);
-		}
+			errorUser("i can't lock mutex");
 		lock=ask;
 		if(pthread_mutex_unlock(&mutex))
-		{
-			if(fprintf (stderr, "%s\n","i can't unlock mutex")==-1)
-			{
-				perror(printfErrorMessage);
-			}
-			lock=quit;
-			pthread_exit(&bad);
-		}
+			errorUser("i can't unlock mutex");
 		while(lock==ask);
-		//fprintf(stdout, "%s\n","i this");
-		for(i=0;  i<count; i++)
+		if(lock<3)
 		{
-			for(j=0;  j<count; j++)
+			for(i=0;  i<count; i++)
 			{
-				if(printf("%c", matrs[lock][i][j])==-1)
+				for(j=0;  j<count; j++)
 				{
-					perror(printfErrorMessage);
-					lock=quit;
+					if(printf("%c", matrs[lock][i][j])==-1)
+						errorUser(printfErrorMessage);
 				}
+				if(printf("\n")==-1)
+					errorUser(printfErrorMessage);
 			}
 			if(printf("\n")==-1)
-			{
-				perror(printfErrorMessage);
-				lock=quit;
-			}
+				errorUser(printfErrorMessage);
+			lock=none;
+			key = mygetch();
 		}
-		if(printf("\n")==-1)
+		else
 		{
-			perror(printfErrorMessage);
-			lock=quit;
-		}	
-		lock=none;
-		key = mygetch();
+			key='q';
+		}
 	}
 	lock=quit;
 }
@@ -117,6 +121,7 @@ char reCount(int i, int j, int current)
 		if(j+1<count && matrs[current][i+1][j+1]=='*')
 			c++;
 	}
+	//fprintf(stdout, "i=%d, j=%d, c=%d\n", i, j, c);
 	if(c<3 || c>6)
 	{
 		return ' ';
@@ -124,9 +129,20 @@ char reCount(int i, int j, int current)
 	return '*';
 }
 
-int reBild(int current, int next)
+void reBild(int current, int next)
 {
 	int i, j;
+	/*
+	for(i=0;  i<count; i++)
+	{
+		for(j=0;  j<count; j++)
+		{
+			printf("%c", matrs[current][i][j]);
+			
+		}
+		printf("\n");
+	}
+	printf("\n");*/
 	for(i=0;  i<count; i++)
 	{
 		for(j=0;  j<count; j++)
@@ -134,16 +150,19 @@ int reBild(int current, int next)
 			matrs[next][i][j]=reCount(i,j,current);
 		}
 	}
-	return 0;
 }
 
 void handler()
 {
 	fprintf(stdout, "%s\n","recount");
+	if(pthread_mutex_lock(&mutex_lock))
+		errorServer("i can't lock mutex");
 	if(next==lock)
 	{
 		next=(next+1)%3;
-	}
+	}	
+	if(pthread_mutex_unlock(&mutex_lock))
+		errorServer("i can't unlock mutex");
 	reBild(current, next);
 	alarm(1);
 	signal (SIGALRM, &handler);
@@ -155,30 +174,24 @@ void *bildLife(void *args)
 	handler();
 	while(lock!=quit)
 	{
-		if(pthread_mutex_lock(&mutex))		// о том почему сдесь стоит мьютекс, вообще он тут не нужен т.к. потоки не конкурентные
-		{									//когда 1 пишет 2ой всегда читает, причем юзер ставится в активное ожидание - да я в 
-			if(fprintf (stderr, "%s\n","i can't lock mutex")==-1)
-			{								// курсе что вы этого не любите :-) а лок юзается для принудительного обновления кэша
-				perror(printfErrorMessage);	//(если вдруг, а скорее всего так оно и есть, переменная ушла в кэш процессора)	
-			}								//в данной задаче этот лок ни на что не влияет, кроме возможного времени отклика клиента
-			key='q';						//вообще этот лок появился как результат опыта написания многопользовательской жизни
-			lock=ask;
-			pthread_exit(&bad);
-		}							
+		if(pthread_mutex_lock(&mutex))
+			errorServer("i can't lock mutex");
+		/*о том почему сдесь стоит мьютекс, вообще он тут не нужен т.к. потоки не конкурентные
+		когда 1 пишет 2ой всегда читает, причем юзер ставится в активное ожидание - да я в 
+		курсе что вы этого не любите :-) а лок юзается для принудительного обновления кэша
+		(если вдруг, а скорее всего так оно и есть, переменная ушла в кэш процессора)	
+		в данной задаче этот лок ни на что не влияет, кроме возможного времени отклика клиента
+		вообще этот лок появился как результат опыта написания многопользовательской жизни*/
 		if(lock==ask)				
-		{							
+		{						
+			if(pthread_mutex_lock(&mutex_lock))
+				errorServer("i can't lock mutex");
 			lock=current;			
+			if(pthread_mutex_unlock(&mutex_lock))
+				errorServer("i can't unlock mutex");
 		}							
 		if(pthread_mutex_unlock(&mutex))  
-		{
-			if(fprintf (stderr, "%s\n","i can't unlock mutex")==-1)
-			{
-				perror(printfErrorMessage);
-			}
-			key='q';
-			lock=ask;
-			pthread_exit(&bad);
-		}
+			errorServer("i can't unlock mutex");
 	}
 	signal (SIGALRM, SIG_DFL);
 }
@@ -196,95 +209,67 @@ void init(int k)
 	}
 }
 
+void freeMatr(int i, int j)
+{
+	int k=j-1, l=i;
+	if(j==0)
+	{
+		free(matrs[l]);
+	}
+	else if(j!=count)
+	{
+		for(; k>=0; k--)
+		{
+			free(matrs[l][k]);
+		}
+		free(matrs[l]);
+	}
+	for(l=i-1; l>=0; l--)
+	{
+		for(k=0; k<count; k++)
+			free(matrs[l][k]);
+		free(matrs[l]);
+	}
+	free(matrs);
+}
+
 int main(int argc,char *argv[])
 {
-	//time_t t1 = time(NULL);
 	pthread_t thread1, thread2;
-	cpu_set_t cpu1, cpu2;
 	if(argc!=2)
-	{
-		if(fprintf (stderr, "%s\n","need count")==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-   	}
+		errorMain("need count");
 	if(!sscanf(argv[1], "%d", &count))
-	{
-		if(fprintf(stderr, "%s %s\n","i, can not cast", argv[1])==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-	}
+		errorMain("i, can not cast argument 1");
 	int i,j,k;
 	matrs = (int ***)malloc(3*sizeof(int **));
 	if(matrs==NULL)
-	{
-		if(fprintf (stderr, "%s\n","no enough memory")==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-	}
+		errorMain("no enough memory");
 	for(i=0; i<3; i++)
 	{
 		matrs[i] = (int **)malloc(count*sizeof(int *));
 		if(matrs[i]==NULL)
 		{
-			if(fprintf (stderr, "%s\n","no enough memory")==-1)
-			{
-				perror(printfErrorMessage);
-			}
-			return EXIT_FAILURE;
+			freeMatr(i,0);
+			errorMain("No enough memory");
 		}	
 		for(j=0; j<count; j++)
 		{
 			matrs[i][j]=(int *)malloc(count*sizeof(int));
 			if(matrs[i][j]==NULL)
 			{
-				if(fprintf (stderr, "%s\n","no enough memory")==-1)
-				{
-					perror(printfErrorMessage);
-				}
-				return EXIT_FAILURE;
+				freeMatr(i,j);
+				errorMain("No enough memory");
 			}
 		}
 	}
 	init(0);
     if (pthread_create(&thread1, NULL, userInt, NULL) || pthread_create(&thread2, NULL, bildLife,NULL))
-    {
-		if(fprintf (stderr, "%s\n","i can't create thread")==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-    }
+		errorMain("i can't create thread");
     if (pthread_join(thread1, NULL) || pthread_join(thread2, NULL))
-    {
-		if(fprintf (stderr, "%s\n","i can't join thread")==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-    }
-	for(i=0; i<3; i++)
-	{
-		for(j=0; j<count; j++)
-		{
-			free(matrs[i][j]);
-		}
-		free(matrs[i]);
-	}
-	free(matrs);
-	if(pthread_mutex_destroy(&mutex))
-	{
-		if(fprintf (stderr, "%s\n","i can't destroy mytex")==-1)
-		{
-			perror(printfErrorMessage);
-		}
-		return EXIT_FAILURE;
-	}
+		errorMain("i can't join thread");
+	freeMatr(2, count-1);
+	if(pthread_mutex_destroy(&mutex) || pthread_mutex_destroy(&mutex_lock))
+		errorMain("i can't destroy mutex");
     return EXIT_SUCCESS;
 }
 
